1050adac9d60c6016b872b38210bb7ab49274cc4
Maria Clarissa Dolor <23-1-02027@vsu.edu.ph>
2025-12-14 15:18:18 +0800

    initial page

---
+++ app/(dashboard)/attendees/page.tsx
@@ -0,0 +1,8 @@
+export default function AttendeesPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-[20px] font-medium text-white mb-6">Attendees</h1>
+      {/* Attendees content */}
+    </div>
+  );
+}
