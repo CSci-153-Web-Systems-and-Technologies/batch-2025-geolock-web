6d9c4dcd9a174e518b9276d7bf48f55074e8c6e4
Maria Clarissa Dolor <23-1-02027@vsu.edu.ph>
2025-12-14 15:19:04 +0800

    initial report page

---
+++ app/(dashboard)/reports/page.tsx
@@ -0,0 +1,8 @@
+export default function ReportsPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-[20px] font-medium text-white mb-6">Reports</h1>
+      {/* Reports content */}
+    </div>
+  );
+}
